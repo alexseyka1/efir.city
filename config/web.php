8425303f24ea45daee3d9674f4e4997126a8106d
alexseyka1 <alexseyka1@gmail.com>
2016-07-14 21:29:41 +0300

    Настроен apache .htaccess. Установлен dektrium users модуль

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $config = [
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
+            'baseUrl' => '',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'V4ePf1B4nZ8LLSicjfz3xBFPO3gOJ-ui',
         ],
@@ -38,14 +39,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
